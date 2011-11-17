3be318c8a3216e8c64d33f80c6a51e3d4a678e13
takkanm <takkanm@gmail.com>
2011-11-17 15:03:33 +0900

    Update select_duty.rb

--- select_duty.rb
+++ select_duty.rb
@@ -56,7 +56,7 @@ end
 if __FILE__ == $0
   File.open(DUTY_LIST, 'w') do |f|
    create_duty( select_duty_members).each do |duty|
-    f.puts duty.join(',')
+     f.puts duty.join(',')
    end
   end
 end
